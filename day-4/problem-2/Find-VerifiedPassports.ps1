63358a0943cdc0172977def909355b96d7640a22
raptors06 <richard.j.breiten@gmail.com>
2021-05-26 22:59:11 -0500

    update patching and naming

--- day-4/problem-2/Find-VerifiedPassports.ps1
+++ day-4/problem-2/Find-VerifiedPassports.ps1
@@ -1,16 +1,28 @@
 <#
     .SYNOPSIS
-        Given an input text file of paths down a hill, find the number of trees on the route
+        Given an input file of possible passport values, determine the number of passports where all required fields are both
+        present and valid according to specific rules.
 
     .DESCRIPTION
-        Given an input text file of paths, determine the number of trees on the route down the hill, assuming a path downhill
-        of 3 moves right for every line moved down.
+        Given an input file of possible passport values, determine the number of passports where all require fields are both
+        present and valid according to the following rules:
 
-    .PARAMETER SuppliedMap
-        Mandatory parameter of "map" supplied via text format from AoC website at https://adventofcode.com/2020/day/3/input
+            * `byr` (Birth Year) - four digits; at least `1920` and at most `2002`.
+            * `iyr` (Issue Year) - four digits; at least `2010` and at most `2020`.
+            * `eyr` (Expiration Year) - four digits; at least `2020` and at most `2030`.
+            * `hgt` (Height) - a number followed by either `cm` or `in`:
+                * If `cm`, the number must be at least `150` and at most `193`.
+                * If `in`, the number must be at least `59` and at most `76`.
+            * `hcl` (Hair color) - a `#` followed by exactly six characters `0`-`9` or `a`-`f`
+            * `ecl` (Eye color) - exactly one of: `amb` or `blu` or `brn` or `gry` or `grn` or `hzl` or `oth`.
+            * `pid` (Passport ID) - a nine-digit number, includig leading zeroes
+            * `cid` (Country ID) - ignore, missing or not
+
+    .PARAMETER SuppliedPassports
+        Mandatory parameter of passports supplied via text format from AoC website at https://adventofcode.com/2020/day/4/input
 
     .EXAMPLE
-        Find-ValidPassports.ps1
+        Find-VerifiedPassports.ps1
 
     .EXAMPLE
         Find-ValidPassports.ps1 -suppliedPassports fubar.txt
@@ -18,7 +30,7 @@
     .NOTES
         Author: Richard J. Breiten
         Version: 7.0-00001
-        Created: 2020-12-12
+        Created: 2021-05-26
         Modified:
 #>
 
